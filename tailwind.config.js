314ecb0ca4aaefbbfc1c3c413bd494910f665bcd
uniS2 <s2oie1010@gmail.com>
2023-07-28 17:34:15 +0900

    chore. tailwind 설정 추가
    config 파일 - view port 사이즈 추가
    
    Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 module.exports = {
   content: ["./client/**/*.html"],
   theme: {
-    screens: {
-      s: { min: "320px", max: "767px" },
-
-      m: { min: "768px", max: "1279px" },
-
-      l: { min: "1280px", max: "1920px" },
-    },
     extend: {
-      fontFamily: {
-        sans: ["Pretendard", "Arial", "sans-serif"],
-      },
       colors: {
         white: "#fff",
         black: "#000",
