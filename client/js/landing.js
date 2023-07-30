0def1aa7d5ba54f86e9aa239564468f5fa3ec52e
HYHYJ <jjangdaram@naver.com>
2023-07-31 00:29:08 +0900

    Feat : landing 페이지 section3 swiper-slide 작업 완료
    landing 페이지 section3 swiper-slide 작업 완료, 관련해 css 변경, section3에 img 삭제
    
    Resolves : #27

--- client/js/landing.js
+++ client/js/landing.js
@@ -19,3 +19,62 @@ const swiper1 = new Swiper(".swiper1", {
   //   el: ".swiper-scrollbar",
   // },
 });
+
+// t = $(this);
+// t.addClass('swiepr-' + index);
+
+const swiper2 = new Swiper(".swiper2", {
+  // autoplay: {
+  //   delay: 0, //add
+  //   disableOnInteraction: false,
+  // },
+  // speed: 7000,
+  // loop: true,
+  // loopAdditionalSlides: 1,
+  // // slidesPerView: ,
+  // slidesPerView: "auto",
+  // centeredSlides: true,
+  // loop: true,
+  // loopedSlides: true,
+  // autoplay: true,
+  autoplay: {
+    delay: 0,
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
+  },
+  speed: 7000,
+  loop: true,
+  slidesPerView: "auto",
+  loopedSlides: 5, //noSwiping : true,
+  observer: true,
+  observeParents: true,
+  spaceBetween: 10,
+});
+
+const swiper3 = new Swiper(".swiper3", {
+  // autoplay: {
+  //   delay: 0, //add
+  //   disableOnInteraction: false,
+  // },
+  // speed: 7000,
+  // loop: true,
+  // loopAdditionalSlides: 1,
+  // // slidesPerView: ,
+  // slidesPerView: "auto",
+  // centeredSlides: true,
+  // loop: true,
+  // loopedSlides: true,
+  // autoplay: true,
+  autoplay: {
+    delay: 0,
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
+  },
+  speed: 6000,
+  loop: true,
+  slidesPerView: "auto",
+  loopedSlides: 5, //noSwiping : true,
+  observer: true,
+  observeParents: true,
+  spaceBetween: 10,
+});
