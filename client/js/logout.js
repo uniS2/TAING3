c2d1559a4dc30955f0ecbead1fe33a0698004118
uniS2 <s2oie1010@gmail.com>
2023-07-31 02:56:14 +0900

    feat. 로그아웃 영역 페이지 js 작업 - 1차 수정
    버튼 영역 html -> js 파일
    
    Related to #29

--- client/js/logout.js
+++ client/js/logout.js
@@ -1,14 +1,12 @@
-import { getNode, getNodes } from "../lib/index.js";
+import { getNode, getNodes, isNull } from "../lib/index.js";
 
 const logout = getNode(".logout__buttons");
-
 const [btnYes, btnNo] = getNodes(".logout__buttons > button");
-
-// function name(params) {}
 
 const handlerLogout = (e) => {
   e.preventDefault();
   const target = e.target.closest("button");
+  if (isNull(target)) return;
 
   if (target === btnYes) window.location.replace("./landing.html");
   //$ 사용자 정보 제거 필요
